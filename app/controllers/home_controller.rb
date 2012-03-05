76b3cf20d7220b745ea568fd9a0d64833a9c307f
Casey Watts <casey.s.watts@gmail.com>
2012-03-04 21:29:20 -0500

    got etherpad js working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   def index
-    require 'etherpad-lite'
+#    require 'etherpad-lite'
 
 # Connect to your Etherpad Lite instance
  ether = EtherpadLite.connect(:local, '8JELX2nMKQOIWbr1W7gN7EZyWlFaoQrq')
 
 # Get a Pad (or create one if it doesn't exist)
-@pad = ether.pad('my first etherpad lite pad')
+@pad = ether.pad('test')
 
 @pad.text = "This is the entire contents of the first pad!"
 
