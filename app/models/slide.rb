c73a1c0bb63573083e9246b6cdb0bdc8e765d771
Casey Watts <casey.s.watts@gmail.com>
2012-03-10 15:41:04 -0500

    added image attachment to slide directly (not gonna use slideimage model? paperclip is alreday pretty abstract)

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,7 +1,8 @@
 class Slide < ActiveRecord::Base
   belongs_to :presentation
-
   #validates_presence_of :presentation_id
   attr_accessor :image
+
+  has_attached_file :image, :styles => { :large => "600x600>", :thumb => "100x100>" }
 
 end
