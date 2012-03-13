37692c85e723e5cf757b85cf759a6ab60766612a
Casey Watts <casey.s.watts@gmail.com>
2012-03-13 15:23:52 -0400

    cleaned up the js

--- app/assets/javascripts/presentations.js
+++ app/assets/javascripts/presentations.js
@@ -2,8 +2,10 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 $(document).ready(function(){
+  $('#container').children().first().css('left', '0%')
 
   // we use code adapted from http://stackoverflow.com/questions/4741880/slide-a-div-offscreen-using-jquery to make our divs move.
+  //Clicking on the slide progresses it forward (we may want to remove this specific functionality~)
   $('.box').click(function() {
       $(this).animate({
          left: '-50%'
@@ -18,14 +20,34 @@ $(document).ready(function(){
      }, 500);
   });
 
+
+
+//PgUp and PgDn move the presentation forward & backward
 $(document).keyup(function (e) {
-  if(e.which == 33) { //pageup
-    
-  }
-  if(e.which == 34) { //pagedown
-    $('#container'). //child? .first
+
+  if(e.keyCode == 34) { //pagedown
+    $('#container').children().first().animate({ //we should store this member in a variable, since we're manipulating it twice
+      left: '-50%'
+      }, 500, function() {
+      $(this).css('left', '-150%');
+      $(this).appendTo('#container');
+      });
+    $('#container').children().first().next().animate({
+     left: '0%'
+     }, 500);
   }
 
+ if(e.keyCode == 33) { //pageup
+    $('#container').children().last().animate({
+      left: '50%'
+      }, 500, function() {
+      $(this).css('left', '150%');
+      $(this).prependTo('#container');
+      });
+    $('#container').children().last().next().animate({ //change "last" to "~previous"
+     left: '0%'
+     }, 500);
+  }
 
 });
 
