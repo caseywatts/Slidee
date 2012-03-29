14335525c5e0e4b3b60eb937b4be70f3ee028794
Casey Watts <casey.s.watts@gmail.com>
2012-03-28 22:24:09 -0400

    added exploratory and broken lambda search

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :presentations
   has_and_belongs_to_many :notegroups
+  #named_scope :all_presentations, lambda {
+    #{
+      #:include => :presentations,
+      #:conditions => [ "d IN (?)", Presentations.map(&:id) ]
+    #}
+  #}
 end
