db0466d5d9fbd997d7c67f0733057a0b25924135
Casey Watts <casey.s.watts@gmail.com>
2012-03-31 13:02:41 -0400

    added small list of users under each presentation

--- app/models/notegroup.rb
+++ app/models/notegroup.rb
@@ -2,4 +2,10 @@ class Notegroup < ActiveRecord::Base
   has_and_belongs_to_many :users
   belongs_to :presentation
   attr_accessor :users_to_add
+
+def everyonebutme
+  list = self.users - [@current_user]
+end
+
+
 end
