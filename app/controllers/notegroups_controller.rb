a3cf3207c0dd1f83652a8aa0148f9002041c9b22
Casey Watts <casey.s.watts@gmail.com>
2012-04-04 22:46:23 -0400

    added sample etherpadAPI code

--- app/controllers/notegroups_controller.rb
+++ app/controllers/notegroups_controller.rb
@@ -13,7 +13,25 @@ class NotegroupsController < ApplicationController
   # GET /notegroups/1
   # GET /notegroups/1.json
   def show
+    #require 'etherpad-lite'
+    ether = EtherpadLite.connect('http://slideeetherpad.cloudfoundry.com', 'HwcKLaD2oyADLbX6ZO8ZgA719Du9NA17')
     @notegroup = Notegroup.find(params[:id])
+    @group = ether.group("my_app_group_#{@notegroup.id}")
+    #@pads = @group.pads
+    # Map the user to an EtherpadLite Author
+    author = ether.author("my_app_user_#{current_user.id}", :name => current_user.fullname)
+
+    # Get or create a two hour long session for this Author in this Group
+    session[:ep_sessions] ||= {} #this should just happen during login
+    sess = session[:ep_sessions][@group.id] ? ether.get_session(session[:ep_sessions][@group.id]) : @group.create_session(author, 120)
+    if sess.expired?
+      sess.delete
+      sess = @group.create_session(author, 60)
+    end
+    session[:ep_sessions][@group.id] = sess.id
+    # Set the EtherpadLite session cookie. This will automatically be picked up by the jQuery plugin's iframe.
+    cookies[:sessionID] = {:value => sess.id, :domain => "afternoon-dawn-1379.herokuapp.com"}
+
 
     respond_to do |format|
       format.html # show.html.erb
