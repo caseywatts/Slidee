c08310fc89a2a50a570b83295655550bf542d81c
Casey Watts <casey.s.watts@gmail.com>
2012-04-13 10:22:08 -0400

    removed thumbnailing in paperclip

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -3,7 +3,7 @@ class Slide < ActiveRecord::Base
   #validates_presence_of :presentation_id
   #attr_accessor :image
 
-  has_attached_file :image, :styles => { :large => "600x600>", :thumb => "100x100>" },
+  has_attached_file :image, :styles => {},
     :storage => :s3,
     :s3_credentials => "config/s3_credentials.yaml",
     :path => ":attachment/:id/:style.:extension",
